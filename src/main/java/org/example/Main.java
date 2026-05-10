d5888ed4ab93cbf8ea026ba21eb0e8397a307b02
jyotibharti87 <joybharti87@gmail.com>
2026-05-09 22:31:31 -0500

    initial-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,16 +13,32 @@ public class Main {
 
         // Use the static methods you wrote in the Calculator class
         // to assign proper values to the four variables below
-        // (instead of 0)
-        int sum = 0;
-        int difference = 0;
-        int product = 0;
-        int quotient = 0;
+
+        // Using StringUtilities methods
+        boolean shortFive = StringUtilities.shortString("launchCode");
+        System.out.println(shortFive);
+
+        char letterFirst = StringUtilities.firstLetter("launchCode");
+        System.out.println(letterFirst);
+
+        String vegReplace =  StringUtilities.censorAsparagus("I don't like to eat asparagus 🤢!");
+        System.out.println(vegReplace);
+
+        String biggerString = StringUtilities.bigger("launch" , "code");
+        System.out.println(biggerString);
+
+
+        // Using Calculator class methods
+        int sum = Calculator.add(a, b);
+        int difference = Calculator.subtract(a, b);
+        int product = Calculator.multiply(a ,b);
+        int quotient = Calculator.divide(a , b);
 
         System.out.println("The sum is " + sum);
         System.out.println("The difference is " + difference);
         System.out.println("The product is " + product);
         System.out.println("The quotient is " + quotient);
     }
+
 
 }
